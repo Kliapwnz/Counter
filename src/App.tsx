fd23363f6b75953a7a8b0af869d2f8ec2e69d7e4
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-31 15:17:45 +0300

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Counter} from "./Counter/Counter";
 
 
 function App() {
+    let [count, setCount] = useState(0)
+    let addCount = () =>{
+        setCount(count +1);
+    }
+    let resetCount = () =>{
+        setCount(count = 0)
+    }
+
     return (
         <div className="App">
             <Counter/>
--- src/Counter/Monitor/Monitor.tsx
+++ src/Counter/Monitor/Monitor.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Monitor = () => {
     return (
         <div>
-            <input/>
+            <div className="display"></div>
         </div>
     );
 };
